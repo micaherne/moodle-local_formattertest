24dde73907fc38b753b52c7011c8a1a568784c24
Michael Aherne <michael.aherne@strath.ac.uk>
2015-03-25 14:48:43 +0000

    Improved tests

--- index.php
+++ index.php
@@ -9,26 +9,61 @@ $formattermanager = new \local_formattertest\string_manager_messageformatter($ro
 
 $count = 10000;
 
+$lazy = get_string('lazystring1', 'local_formattertest', null, true);
+
 $testdata = array(
-	'original1' => 'test',
-	'original2' => (object) array('test' => 'test'),
-	'formatter1' => array('what' => 'something', 'dummy' => 'one', 'dummy2' => 'two', 'dummy3' => 'three', 'dummy4' => 'four'),
-	'formatter2' => array('count' => 10000)
+    array(
+        'test',
+        array('what' => 'test')
+    ),
+    array(
+        array('what' => 'test'),
+        array('what' => 'test')
+    ),
+    array(
+        array('one' => 'one', 'two' => 'two', 'three' => 'three', 'four' => 'four'),
+        array('one' => 'one', 'two' => 'two', 'three' => 'three', 'four' => 'four')
+    ),
+    array(
+            (object)array('what' => 'test'),
+            (object)array('what' => 'test')
+    ),
+    array(
+            array('what' => $lazy),
+            array('what' => $lazy)
+    ),
+    array(
+         null,
+         array('count' => 200)
+    )
 );
 
-foreach ($testdata as $string => $a) {
+mtrace("Runs: " . $count);
 
-	mtrace("Test: " . $string);
+purge_all_caches();
+
+foreach ($testdata as $key => $data) {
+
+    $originaldata = $data[0];
+    $formatterdata = $data[1];
+
+	mtrace("Test " . $key);
 
 	$start1 = microtime(true);
 	for($i = 0; $i < $count; $i++) {
-		$x = $originalmanager->get_string($string, 'local_formattertest', $a);
+		$x = $originalmanager->get_string('original' . $key, 'local_formattertest', $originaldata);
 	}
 	mtrace("Original ($x): " . (microtime(true) - $start1));
 
 	$start2 = microtime(true);
 	for($i = 0; $i < $count; $i++) {
-		$x = $formattermanager->get_string($string, 'local_formattertest', $a);
+		$x = $formattermanager->get_string('formatter' . $key, 'local_formattertest', $formatterdata);
 	}
 	mtrace("Formatter ($x): " . (microtime(true) - $start2));
+
+	$start3 = microtime(true);
+	for($i = 0; $i < $count; $i++) {
+	    $x = $formattermanager->get_string('original' . $key, 'local_formattertest', $originaldata);
+	}
+	mtrace("Formatter with original formatting ($x): " . (microtime(true) - $start3));
 }
--- lang/en/local_formattertest.php
+++ lang/en/local_formattertest.php
@@ -1,8 +1,27 @@
 <?php
 
 $string['lazystring1'] = 'lazy string';
-$string['original1'] = 'This is a {$a}';
-$string['original2'] = 'This is a {$a->test}';
 
-$string['formatter1'] = 'I have {what}';
-$string['formatter2'] = 'I have {count, plural, one{# thing} other{# things}}';
+$string['phpunit1'] = 'This is a {$a}';
+$string['phpunit2'] = 'This is a {$a->test}';
+$string['phpunit3'] = 'I have {what}';
+$string['phpunit4'] = 'I have {what}';
+$string['phpunit5'] = 'I have {count, plural, one{# thing} other{# things}}';
+
+$string['original0'] = 'This is a {$a}';
+$string['formatter0'] = 'This is a {what}';
+
+$string['original1'] = 'This is a {$a->what}';
+$string['formatter1'] = 'This is a {what}';
+
+$string['original2'] = '{$a->one}, {$a->two}, {$a->three}, {$a->four}';
+$string['formatter2'] = '{one}, {two}, {three}, {four}';
+
+$string['original3'] = 'This is a {$a->what}';
+$string['formatter3'] = 'This is a {what}';
+
+$string['original4'] = 'This is a {$a->what}';
+$string['formatter4'] = 'This is a {what}';
+
+$string['original5'] = 'Not possible with current system';
+$string['formatter5'] = 'I have {count, plural, one{# thing} other{# things}}';
